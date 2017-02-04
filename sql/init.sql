8aae1f6aaac6f1c87ade14c49827035d4bb90f40
Siphan Bou <35@holbertonschool.com>
2017-02-04 15:42:27 +0100

    Specify where users can access mysql from

--- sql/init.sql
+++ sql/init.sql
@@ -4,8 +4,8 @@
 */
 
 -- Create users for SQL Authentication
-CREATE USER 'airbnb_user_prod'@'localhost' IDENTIFIED BY '';
-CREATE USER 'airbnb_user_dev'@'%' IDENTIFIED BY '';
+CREATE USER 'airbnb_user_dev'@'%' IDENTIFIED BY ''; -- Can access to MySQL from anywhere
+CREATE USER 'airbnb_user_prod'@'localhost' IDENTIFIED BY ''; -- Can access to MySQL from only localhost
 
 -- Create 2 databases in UTF8 (utf8_general_ci)
 CREATE DATABASE airbnb_dev CHARACTER SET utf8 COLLATE utf8_general_ci;
