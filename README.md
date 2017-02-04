8ebffdef2ac8f044ef5310dc67c8e9a281c57f81
Siphan Bou <35@holbertonschool.com>
2017-02-04 15:36:20 +0100

    Create 2 databases to separate dev from prod environments

---
+++ README.md
@@ -0,0 +1,5 @@
+This project is a mini clone of the AirBnb website.
+
+It is under construction and reviewed by peers at Holberton School on a weekly basis.
+
+Each new branch should be merged into Master after latest update has been validated by peer review.
---
+++ sql/init.sql
@@ -0,0 +1,18 @@
+/*
+   As it is crucial to distinguish between development and production environments,
+   this script creates a user and a database specific for each environment.
+*/
+
+-- Create users for SQL Authentication
+CREATE USER 'airbnb_user_prod'@'localhost' IDENTIFIED BY '';
+CREATE USER 'airbnb_user_dev'@'%' IDENTIFIED BY '';
+
+-- Create 2 databases in UTF8 (utf8_general_ci)
+CREATE DATABASE airbnb_dev CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE airbnb_prod CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+-- Add user dev to database dev
+GRANT ALL PRIVILEGES ON database.airbnb_dev TO 'airbnb_user_dev'@'%' IDENTIFIED BY '';
+
+-- Add user prod to database prod
+GRANT ALL PRIVILEGES ON	database.airbnb_prod TO 'airbnb_user_prod'@'localhost' IDENTIFIED BY '';
