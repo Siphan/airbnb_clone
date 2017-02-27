374776eb238b979bf7f958ea68b06ce2992ab9cf
Siphan Bou <35@holbertonschool.com>
2017-02-27 23:57:21 +0100

    Create our base class inherited from peewee.Model

---
+++ api/app/models/base.py
@@ -0,0 +1,29 @@
+"""
+    This base module defines the model that will be the base
+    of all our classes. It is itself inherited from a Model
+    from peewee, a Python object relational mapper (ORM).
+"""
+
+import peewee
+from config import *
+from datetime import datetime
+
+database = peewee.MySQLDatabase( DATABASE['database'],
+                          user=DATABASE['user'],
+                          charset=DATABASE['charset'],
+                          host=DATABASE['host'],
+                          port=DATABASE['port'],
+                          passwd=DATABASE['password'] )
+
+class BaseModel(peewee.Model):
+    id = peewee.PrimaryKeyField(unique=True)
+    created_at = peewee.DateTimeField(default=datetime.now, formats='%Y/%m/%d %H:%M:%S')
+    updated_at = peewee.DateTimeField(default=datetime.now, formats='%Y/%m/%d %H:%M:%S')
+
+    def save(self, *args, **kwargs):
+        self.updated_at = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+        peewee.Model.save(self)
+
+    class Meta:
+        database = database
+        order_by = ("id", )
