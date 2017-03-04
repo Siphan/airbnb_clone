9ff8febbeff19de524a37856c0d63a56be242d8b
Siphan Bou <35@holbertonschool.com>
2017-03-04 18:32:17 +0100

    Create Flask app and make it global to Python app

---
+++ api/app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_json import FlaskJSON
+import config
+
+__all__ = ["config"]
+
+# Initialize Flask application
+app = Flask(__name__)
+
+# Initialize FlaskJSON instance with Flask app
+FlaskJSON(app)
+
+"""Import all views"""
+from views import *
