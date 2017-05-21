033b758fc9084df51edb2a9137aa2770cef8de56
Siphan <siphan.bou@gmail.com>
2017-05-21 16:21:16 +0200

    Connect app to database using flask-mysqldb

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,15 +1,21 @@
 """
  This script creates a Flask application
- and makes it global to our Python app
- """
+ and makes it global to our Python app.
+ It also adds a MySQL instance to our code.
+"""
 from flask import Flask
 from flask_json import FlaskJSON
 import config
+from flask_mysqldb import MySQL
 
 __all__ = ["config"]
 
-# Initialize Flask application
+"""
+Initialize Flask application
+An instance of this class will be our WSGI application.
+"""
 app = Flask(__name__)
+mysql = MySQL(app)
 
 # Initialize FlaskJSON instance with Flask app
 FlaskJSON(app)
--- api/app/views/index.py
+++ api/app/views/index.py
@@ -24,6 +24,14 @@ def index():
     return json_response(status="OK", utc_time=datetime.utcnow().strftime('%m/%d/%Y %H:%M:%S'), time=utc_to_local(datetime.utcnow()).strftime('%m/%d/%Y %H:%M:%S')) #format 2nd arg into UTC
 
 
+@app.route('/')
+def users():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT user, host FROM mysql.user''')
+    rv = cur.fetchall()
+    return str(rv)
+
+
 @app.errorhandler(404)
 def not_found(e):
     """Function to manage all not found routes"""
