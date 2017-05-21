7d3d8a752031a32cb65ce8baea0903bc49edd342
Siphan <siphan.bou@gmail.com>
2017-05-21 15:31:29 +0200

    Add script for creating test user and database

---
+++ sql/unit_test.sql
@@ -0,0 +1,11 @@
+# Create user and database for testing of AirBnb Clone.
+
+# Create user airbnb_user_test that can access the database from any IP.
+CREATE USER 'airbnb_user_test'@'%' IDENTIFIED BY 'airbnb_user_test';
+
+# Create the database.
+CREATE DATABASE airbnb_test
+DEFAULT COLLATE utf8_general_ci;
+
+# Grant privileges to the database for test user.
+GRANT ALL ON airbnb_test.* TO 'airbnb_user_test'@'%' IDENTIFIED BY 'airbnb_user_test';
