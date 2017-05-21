bea4cf0f51bb4955b0a43d911b94408b00ba490e
Siphan <siphan.bou@gmail.com>
2017-05-21 17:01:01 +0200

    Pass first unit tests

--- api/app/views/index.py
+++ api/app/views/index.py
@@ -1,51 +1,41 @@
+"""Defines the flask function to run when a GET request is made to the root
+directory '/'. Handles the behavior for a 404 error.
 """
-This module allows only GET requests to be called on our API.
-Response returns UTC time and local time of server.
-HTTP response code 404 is handled.
-"""
-
-from flask_json import FlaskJSON, json_response
-from datetime import datetime, tzinfo
-from pytz import utc, timezone
-from app import app, models
+from flask import Flask, jsonify, request
+from flask_json import FlaskJSON, JsonError, json_response, as_json
+from app import app
+import datetime
+import time
+from app.models.base import BaseModel
 from peewee import *
 
-local_tz = timezone('America/Los_Angeles')
 
 @app.route('/', methods=['GET'])
+@as_json
 def index():
+    """Return a JSON object with the status code of the server, and UTC time
+    and the server time.
     """
-    Function to manage the API route /
-    allowing only GET request
-    response will be a JSON
-    return a hash with keys/values:
-    status 'OK', utc_time and time
-    """
-    return json_response(status="OK", utc_time=datetime.utcnow().strftime('%m/%d/%Y %H:%M:%S'), time=utc_to_local(datetime.utcnow()).strftime('%m/%d/%Y %H:%M:%S')) #format 2nd arg into UTC
+    return jsonify(status="OK",
+                   utc_time=datetime.datetime
+                                    .utcnow()
+                                    .strftime("%Y/%m/%d %H:%M:%S"),
+                   time=datetime.datetime
+                                .now()
+                                .strftime("%Y/%m/%d %H:%M:%S"))
+
+def before_request():
+    """Connect to the database defined in BaseModel."""
+    BaseModel.database.connect()
 
 
-@app.route('/')
-def users():
-    cur = mysql.connection.cursor()
-    cur.execute('''SELECT user, host FROM mysql.user''')
-    rv = cur.fetchall()
-    return str(rv)
+def after_request():
+    """Close the connection to the database defined in the BaseModel."""
+    BaseModel.database.close()
 
 
 @app.errorhandler(404)
-def not_found(e):
-    """Function to manage all not found routes"""
-    return json_response(add_status_=False, code=404, msg="not found")
-
-def utc_to_local(utc_dt):
-    """Function to convert UTC to local server time"""
-    local_dt = utc_dt.replace(tzinfo=utc).astimezone(local_tz)
-    return local_tz.normalize(local_dt)
-
-def before_request():
-    """Function to open a database connection before each request"""
-    models.database.connect()
-
-def after_request():
-    """Function to close a database connection after each request"""
-    models.database.close()
+def page_not_found(error):
+    """Return a JSON object with the code 404, and a message "not found"."""
+    return jsonify(code=404,
+                   msg="not found")
