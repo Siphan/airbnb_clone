8c24a3fb16ac045ea07a44fa0dc9662e6932b5a5
Siphan Bou <sbou@vente-privee.com>
2017-05-23 17:02:44 +0200

    Add comments to explain script that initializes our Flask app

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,3 +1,7 @@
+"""
+    This script initializes our Flask application by creating
+    an instance of the Flask class that will be our WSGI application.
+"""
 from flask import Flask
 from flask_json import FlaskJSON
 import os
